ab0cd7dc2866c35e8e4ca050796ed20ff01ed6da
Bruno Dantas <brunoodantas7@gmail.com>
2022-07-13 23:53:46 -0300

    Create header component and styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
 import NotificationButton from "./components/NotificationButton";
+import Header from "./components/Header";
 
 function App() {
 
     return (
         <>
             <h1>Hello World!</h1>
+            <Header/>
+            <NotificationButton/>
             <NotificationButton/>
         </>
     )
