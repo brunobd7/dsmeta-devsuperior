c3d09caee59ea661f522ad3aacab6731179ef996
Bruno Dantas <brunoodantas7@gmail.com>
2022-07-15 00:32:15 -0300

    Mapped repo, resource and service.Get endpoint with pageable sample

---
+++ backend/dsmeta/src/main/java/com/devsuperior/dsmeta/resources/SaleResource.java
@@ -0,0 +1,25 @@
+package com.devsuperior.dsmeta.resources;
+
+import com.devsuperior.dsmeta.entities.Sale;
+import com.devsuperior.dsmeta.services.SaleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/sales")
+public class SaleResource {
+
+    @Autowired
+    private SaleService service;
+
+    @GetMapping
+    public Page<Sale> findSales(Pageable pageable){
+        return service.findSales(pageable);
+    }
+}
---
+++ backend/dsmeta/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -0,0 +1,26 @@
+package com.devsuperior.dsmeta.services;
+
+import com.devsuperior.dsmeta.entities.Sale;
+import com.devsuperior.dsmeta.repositories.SaleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SaleService {
+
+    @Autowired
+    private SaleRepository saleRepository;
+
+    public List<Sale> findSales(){
+        return saleRepository.findAll();
+    }
+
+    public Page<Sale> findSales(Pageable pageable){
+        return saleRepository.findAll(pageable);
+    }
+
+}
