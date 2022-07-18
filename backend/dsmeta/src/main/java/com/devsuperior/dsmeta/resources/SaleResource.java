5e8e6d76e13676762c4bd2aea97eb21dd8fdef41
Bruno Dantas <brunoodantas7@gmail.com>
2022-07-17 22:52:49 -0300

    Add Twilio dependency and done an implementation of simple SMS notification message

--- backend/dsmeta/src/main/java/com/devsuperior/dsmeta/resources/SaleResource.java
+++ backend/dsmeta/src/main/java/com/devsuperior/dsmeta/resources/SaleResource.java
@@ -1,14 +1,12 @@
 package com.devsuperior.dsmeta.resources;
 
 import com.devsuperior.dsmeta.entities.Sale;
+import com.devsuperior.dsmeta.services.SMSService;
 import com.devsuperior.dsmeta.services.SaleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -20,10 +18,20 @@ public class SaleResource {
     @Autowired
     private SaleService service;
 
+    @Autowired
+    private SMSService smsService;
+
     @GetMapping
     public Page<Sale> findSales(@RequestParam(value = "minDate") String minDate,
                                 @RequestParam(value = "maxDate") String maxDate,
                                 Pageable pageable){
         return service.findSales(minDate, maxDate, pageable);
+    }
+
+
+    @GetMapping("/{id}/notification")
+    public void notifySms(@PathVariable Long id){
+
+        smsService.sendSms(id);
     }
 }
